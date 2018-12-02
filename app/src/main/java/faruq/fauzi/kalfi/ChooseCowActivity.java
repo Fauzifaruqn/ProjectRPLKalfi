5f2a9143af8aa7ff1c81c2677c803c30c339a6cc
Rifqy <mrifqyaa@gmail.com>
2018-12-02 19:05:52 +0700

    PilihSapiYangAkanDiinsemnisi
    Masih menunggu beberapa gambar dari iyon

---
+++ app/src/main/java/faruq/fauzi/kalfi/ChooseCowActivity.java
@@ -0,0 +1,13 @@
+package faruq.fauzi.kalfi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChooseCowActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_cow);
+    }
+}
